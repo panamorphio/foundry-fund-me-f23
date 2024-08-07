7679404e15ce88a75f892bb66afac5ebf86e48e0
panamorphio <daniil.kahri@gmail.com>
2024-08-07 17:09:59 +0300

    My first commit

---
+++ script/Interactions.s.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+// Fund
+// Withdraw
+
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.1 ether;
+
+    function fundFundMe(address mostRecentlyDeployed) public payable {
+        vm.prank(msg.sender);
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        vm.startBroadcast();
+        fundFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+
+}
+
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.prank(msg.sender);
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        console.log("Withdrew funds");
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        vm.startBroadcast();
+        withdrawFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+
+}
